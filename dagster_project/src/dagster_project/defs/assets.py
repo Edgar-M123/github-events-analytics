ad3655c14b41096c7b1c9c2f343c72d300307813
edgar <edgar.ad.martinez@gmail.com>
2026-05-01 20:53:39 -0400

    Defined GitHub event schema for loading into BigQuery

--- dagster_project/src/dagster_project/defs/assets.py
+++ dagster_project/src/dagster_project/defs/assets.py
@@ -1,103 +1,104 @@
 import dagster as dg
-import pandas as pd
-from dagster_duckdb import DuckDBResource
+from dagster_gcp import BigQueryResource
 import datetime as dt
+from google.cloud import bigquery as bq
+import requests
+import io
+import gzip
 
 @dg.asset
-def gh_events_json_gz(duckdb: DuckDBResource):
+def gh_events_json_gz(bigquery: BigQueryResource):
 
     dates = [(dt.datetime(year=2026, month=1, day=1) + dt.timedelta(hours=x)).strftime('%Y-%m-%d-%-H') for x in range(3)]
     table_name = "raw.github_events"
 
-    with duckdb.get_connection() as conn:
+
+    json_schema = [
+        bq.SchemaField("id", "STRING"),
+        bq.SchemaField("type", "STRING"),
+        bq.SchemaField("actor", "STRUCT", fields=[
+            bq.SchemaField("id", "STRING"),
+            bq.SchemaField("login", "STRING"),
+            bq.SchemaField("display_login", "STRING"),
+            bq.SchemaField("gravatar_id", "STRING"),
+            bq.SchemaField("url", "STRING"),
+            bq.SchemaField("avatar_url", "STRING"),
+        ]),
+        bq.SchemaField("repo", "STRUCT", fields=[
+            bq.SchemaField("id", "STRING"),
+            bq.SchemaField("name", "STRING"),
+            bq.SchemaField("url", "STRING"),
+        ]),
+        bq.SchemaField("payload", "JSON"),
+        bq.SchemaField("public", "BOOL"),
+        bq.SchemaField("created_at", "TIMESTAMP"),
+        bq.SchemaField("org", "STRUCT", fields=[
+            bq.SchemaField("id", "STRING"),
+            bq.SchemaField("name", "STRING"),
+            bq.SchemaField("url", "STRING"),
+            bq.SchemaField("gravatar_id", "STRING"),
+            bq.SchemaField("avatar_url", "STRING"),
+        ]),
+    ]
+
+
+    with bigquery.get_client() as client:
+        table_ref = client.dataset("raw").table("github_events")
+
         for i, date in enumerate(dates):
             print(f"Reading data for {date}")
             url = f"https://data.gharchive.org/{date}.json.gz"
-            
+            res = requests.get(url)
+            gzip_data = res.content
+
+            json_data = gzip.decompress(gzip_data)
+
             if i == 0:
-                conn.execute(
-                    f"""
-                    create or replace table {table_name} as (
-                        select *, current_date as loaded_on from read_json_auto('{url}', union_by_name=true, map_inference_threshold=0)
-                    );
-                    """
+                job_config = bq.LoadJobConfig(
+                    source_format=bq.SourceFormat.NEWLINE_DELIMITED_JSON,
+                    schema=json_schema,
+                    autodetect=True,
+                    write_disposition=bq.WriteDisposition.WRITE_APPEND,
+                    ignore_unknown_values=True
                 )
+
+                job = client.load_table_from_file(
+                    file_obj=io.BytesIO(json_data),
+                    destination=table_ref,
+                    job_config=job_config
+                )
+                
+                job.result()
+
+
             else:
-                conn.execute(
-                    f"""
-                    insert into {table_name} 
-                    select *, current_date as loaded_on from read_json_auto('{url}', union_by_name=true, map_inference_threshold=0);
-                    """
+                job_config = bq.LoadJobConfig(
+                    source_format=bq.SourceFormat.NEWLINE_DELIMITED_JSON,
+                    schema=json_schema,
+                    write_disposition=bq.WriteDisposition.WRITE_TRUNCATE,
+                    ignore_unknown_values=True
                 )
+
+                job = client.load_table_from_file(
+                    file_obj=io.BytesIO(json_data),
+                    destination=table_ref,
+                    job_config=job_config
+                )
+
+                job.result()
 
 
 @dg.asset_check(asset='gh_events_json_gz')
-def check_events_json_loaded(duckdb: DuckDBResource) -> dg.AssetCheckResult:
+def check_events_json_loaded(bigquery: BigQueryResource) -> dg.AssetCheckResult:
 
     table_name = "raw.github_events"
 
-    with duckdb.get_connection() as conn:
-        row_count = conn.execute(
+    with bigquery.get_client() as client:
+        row_count = client.query(
             f"""
             select count(*) from {table_name}
             """
-        ).fetchone()
-
-        if row_count is None or row_count == 0:
-            return dg.AssetCheckResult(
-                passed = False, metadata={"message": "No data in this table."}
-            )
-
-        return dg.AssetCheckResult(
-            passed = True, metadata={"message": "Data exists in this table."}
-        )
-
-
-@dg.asset
-def customers(duckdb: DuckDBResource):
-    url = "https://raw.githubusercontent.com/dbt-labs/jaffle-shop-classic/refs/heads/main/seeds/raw_customers.csv"
-    table_name = "raw.customers"
-
-    with duckdb.get_connection() as conn:
-        conn.execute(
-            f"""
-            create or replace table {table_name} as (
-                select * from read_csv_auto('{url}')
-            );
-            """
-        )
-
-@dg.asset(
-    deps=["customers"]
-)
-def customers_with_rank(duckdb: DuckDBResource):
-
-    table_name = "int.customers_ranked"
-
-    with duckdb.get_connection() as conn:
-        conn.execute(
-            f"""
-            create or replace table {table_name} as (
-                select
-                    id,
-                    first_name,
-                    last_name,
-                    rank() over (order by last_name) as last_name_order
-                from raw.customers
-            );
-            """
-        )
-
-@dg.asset_check(asset="customers_with_rank")
-def customers_with_rank_row_check(duckdb: DuckDBResource) -> dg.AssetCheckResult:
-    table_name = "int.customers_ranked"
-
-    with duckdb.get_connection() as conn:
-        row_count = conn.execute(
-            f"""
-            select count(*) from {table_name}
-            """
-        ).fetchone()
+        ).result()
 
         if row_count is None or row_count == 0:
             return dg.AssetCheckResult(
--- dagster_project/src/dagster_project/defs/resources.py
+++ dagster_project/src/dagster_project/defs/resources.py
@@ -1,11 +1,18 @@
 import dagster as dg
 from dagster_duckdb import DuckDBResource
+from dagster_gcp import BigQueryResource
 
 database_resource = DuckDBResource(database="../databases/analytics.duckdb")
+
+bigquery_resource = BigQueryResource(
+    project="analytics-engineering-494923",
+    location="US"
+)
 
 
 @dg.definitions
 def resources() -> dg.Definitions:
     return dg.Definitions(resources={
-        "duckdb": database_resource
+        "duckdb": database_resource,
+        "bigquery": bigquery_resource
     })
